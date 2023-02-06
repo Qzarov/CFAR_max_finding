c07c59aaf50901ff05295ef805df36ba10f8033a
Yaroslav Paroshin <qzarov@mail.ru>
2023-02-07 00:26:47 +0300

    Add alghorithm for CFAR maxima finding

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
 void generateMatrix();
+bool isTresholdExceeding( float sb, float ss, float th, float v);
 
 class Matrix {
 public:
 
     class Element {
     public:
-        explicit Element(float _value)
-            : value(_value), col(0), row(0) { }
+        //explicit Element(float _value)
+        //    : value(_value), col(0), row(0) { }
+
+        Element(int _col, int _row)
+            : value(0), col(_col), row(_row) { }
+
         Element(int _col, int _row, float _value)
             : value(_value), col(_col), row(_row) { }
 
@@ -38,9 +44,13 @@ public:
         if (isIndexesOk(el)) { matrix[el.col][el.row] = el.value; }
         else { cerr << "Index isn't OK (setElement)\n"; }
     }
-    float getElement(Element el) const {
+    float getElement(int _col, int _row) const {
+        Element el(_col, _row);
         if (isIndexesOk(el)) { return matrix[el.col][el.row]; }
-        else { cerr << "Index isn't OK (getElement)\n"; }
+        else {
+            cerr << "Index isn't OK (getElement)\n";
+            return el.value;
+        }
     }
 
     float calcRectangleSum(int vSize, int hSize, Element centralEl) const {
@@ -134,15 +144,54 @@ private:
 
 int main() {
     std::cout << "Hello, Vector!" << std::endl;
-    generateMatrix();
-    Matrix matrix(5, 5, "data.txt");
 
-    Matrix::Element el(0, 2, 10.1);
-    cout << matrix.calcRectangleSum(1, 3, el);
+    int x, y;
+    cout << "Enter X and Y: ";
+    cin >> x, y;
+
+    int xb, yb, xs, ys;
+    cout << "Enter Xb and Yb: ";
+    cin >> xb >> yb;
+    cout << "Enter Xs and Ys: ";
+    cin >> xs >> ys;
+
+    const float err = 0.01;
+    int sq = xb * yb - xs * ys;
+    float th = pow((1 / err), (1 / sq));
+
+    string path;
+    cout << "Enter path to file: ";
+    cin >> path;
+    Matrix matrix(x, y, path);
+
+    Matrix sb(x, y), ss(x, y);
+    cout << "Exceeding points (value, row, column)";
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            Matrix::Element el(j, i);
+            el.value = matrix.getElement(j, i);
+            Matrix::Element elSb(j, i, matrix.calcRectangleSum(yb, xb, el));
+            sb.setElement(elSb);
+            Matrix::Element elSs(j, i, matrix.calcRectangleSum(ys, xs, el));
+            sb.setElement(elSs);
+
+            if (abs(elSb.value) < 0.001 || abs(elSs.value) < 0.001) {
+                continue;
+            }
+
+            if (isTresholdExceeding(elSb.value, elSs.value, th, el.value)) {
+                cout << "exceed: " << el.value << " " << el.row << " " << el.col << "\n";
+            }
+        }
+    }
 
     return 0;
 }
 
+bool isTresholdExceeding(const float sb, const float ss,
+                         const float th, const float v) {
+    return (sb - ss) * th < v;
+}
 
 void generateMatrix() {
     float arr[] = {
